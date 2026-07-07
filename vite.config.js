c67343967a0b708c93e2f26e9851cc1ee4ce14bb
Efrain Cuessy <efraincuessy@gmail.com>
2026-07-06 21:18:41 -0600

    fix -- fixed tailwind vite plugin import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("vite");
 const react = require("@vitejs/plugin-react");
-const tailwindcss = require("@tailwindcss/vite");
+const tailwindcss = require("@tailwindcss/vite").default;
 
 module.exports = defineConfig({
   plugins: [react(), tailwindcss()],
